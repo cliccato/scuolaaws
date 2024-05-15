1969aed91ed2fc1e2942fc4d140d458f383bb9bd
cliccato <cliccato@bigdi.cc>
2024-05-15 10:23:59 +0200

    update

--- home.php
+++ home.php
@@ -11,15 +11,45 @@
         <h1 class="text-center">Benvenuto <?php echo $_COOKIE["username"]; ?></h1>
     </div>
 
-    <a>per la relalizzazione ho creato un container php:apache su cui ho installato la libreria mysqli con un docker file andando a buildare un immagine custom</a>
-    <a>ho scaricato un immagine mariadb e un immagine ssl proxy</a>
-    <a>ho linkato il proxy al server web</a>
-    <a>ho eseguito tutti i container</a>
-    <a>ho creato un volume docker in cui salavare i dati del db per non perderli</a>
-    <a>ho creato un network docker a cui ho collegato i container del webserver e del db per poter far accedere in locale il db al server</a>
-    <a>ho creato un certificato ssl di tipo rsa tramite lutilizzo di openssl</a>
-    <a>ho sviluppato il sito web con php html e css</a>
-    <a>ho spostato i file del sito sul server tramite una repository git</a>
+    <div class="container mt-5">
+        <div class="card">
+            <div class="card-header bg-primary text-white text-center">
+                <h2>Progetto Docker con PHP, MariaDB e SSL Proxy</h2>
+            </div>
+            <img src="https://www.itsitio.com/wp-content/uploads/2020/04/banner-logo-aws-1024x403.png" class="card-img-top">
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <strong>1.</strong> Ho creato un container PHP:Apache su cui ho installato la libreria mysqli con un Dockerfile andando a buildare un'immagine custom.
+                    </li>
+                    <li class="list-group-item">
+                        <strong>2.</strong> Ho scaricato un'immagine MariaDB e un'immagine SSL Proxy.
+                    </li>
+                    <li class="list-group-item">
+                        <strong>3.</strong> Ho linkato il proxy al server web.
+                    </li>
+                    <li class="list-group-item">
+                        <strong>4.</strong> Ho eseguito tutti i container.
+                    </li>
+                    <li class="list-group-item">
+                        <strong>5.</strong> Ho creato un volume Docker in cui salvare i dati del DB per non perderli.
+                    </li>
+                    <li class="list-group-item">
+                        <strong>6.</strong> Ho creato un network Docker a cui ho collegato i container del webserver e del DB per poter far accedere in locale il DB al server.
+                    </li>
+                    <li class="list-group-item">
+                        <strong>7.</strong> Ho creato un certificato SSL di tipo RSA tramite l'utilizzo di OpenSSL.
+                    </li>
+                    <li class="list-group-item">
+                        <strong>8.</strong> Ho sviluppato il sito web con PHP, HTML e CSS.
+                    </li>
+                    <li class="list-group-item">
+                        <strong>9.</strong> Ho spostato i file del sito sul server tramite una repository Git.
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
